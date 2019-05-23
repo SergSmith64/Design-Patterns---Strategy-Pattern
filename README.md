17789f6a4b2a2fd178a2ba81607aaaa7e0e45292
SergSmith64 <sergsmith2008@yandex.ru>
2019-05-23 15:59:40 +0400

    commit_01_Create_Task

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Design Patterns - C# - Strategy Pattern</h1>
+
+<p>Education C#</p>
+
+<br>
+<p><b>2019-May-23</b> Create_Task</p>
---
+++ StrategySample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===DESIGN PATTERNS - STRATEGY SAMPLE===");
+            Console.WriteLine("\n=======Lake with different ducks=======");
+            Console.ReadLine();
+        }
+    }
+}
